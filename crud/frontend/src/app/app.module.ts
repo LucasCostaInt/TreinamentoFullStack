c338e2724e875d33f6d1d858daed909077e4b27a
Lucas Costa <62567333+Lucasmirandac@users.noreply.github.com>
2020-12-12 14:30:14 -0300

    Criação do product-create.component
    Criação do componente de criação de produto.

--- crud/frontend/src/app/app.module.ts
+++ crud/frontend/src/app/app.module.ts
@@ -5,14 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/template/header/header.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { ProductCreateComponent } from './components/product/product-create/product-create.component'; 
 import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component';
+import { HomeComponent } from './views/home/home.component';
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import {MatListModule} from '@angular/material/list';
-import { HomeComponent } from './views/home/home.component';
 import {MatCardModule} from '@angular/material/card';
-import { ProductCrudComponent } from './views/product-crud/product-crud.component'; 
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -21,7 +23,8 @@ import { ProductCrudComponent } from './views/product-crud/product-crud.componen
     FooterComponent,
     NavComponent,
     HomeComponent,
-    ProductCrudComponent
+    ProductCrudComponent,
+    ProductCreateComponent
   ],
   imports: [
     MatSidenavModule,
@@ -31,8 +34,7 @@ import { ProductCrudComponent } from './views/product-crud/product-crud.componen
     BrowserAnimationsModule,
     MatToolbarModule,
     MatCardModule,
-    
-
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
